8f39cb189c5df96834e0b5aed864b5385b437091
Bigyan Parajuli <np03cs4a220311@heraldcollege.edu.np>
2023-02-13 12:19:48 +0545

    Initial commit

---
+++ effect.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".typing-1",{
+    strings: [
+         "Developer", "Blogger",
+        "Guitarist"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2",{
+    strings: [
+        "Astrophile", "Developer", "Blogger",
+        "Guitarist"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
